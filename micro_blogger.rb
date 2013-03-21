19cf962a0921b436cc561b790eac8fa106aefd33
Justin Leveck <jleveck@gmail.com>
2013-03-21 22:42:56 +0000

    added method to return klout scores for friends

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,12 +1,14 @@
 require 'jumpstart_auth'
+require 'klout'
 require 'bitly'
-Bitly.use_api_version_3
 
 class MicroBlogger
   attr_reader :client
 
   def initialize
     puts "Initializing..."
+    Bitly.use_api_version_3
+    Klout.api_key = 'xu9ztgnacmjx3bu82warbr3h'
     @client = JumpstartAuth.twitter
   end
 
@@ -16,6 +18,10 @@ class MicroBlogger
 
   def followers_list
     screen_names = @client.followers.collect{|follower| follower.screen_name}
+  end
+
+  def friends_list
+    screen_names = @client.friends.collect{ |f| f.screen_name }
   end
 
   def spam_my_followers(message)
@@ -49,6 +55,16 @@ class MicroBlogger
     bitly.shorten(original_url).short_url
   end  
 
+  def klout_score
+    friends_list.each do |friend|
+      puts "#{friend}'s Klout score is..."
+      identity = Klout::Identity.find_by_screen_name(friend)
+      user = Klout::User.new(identity.id)
+      puts user.score.score
+      puts "\n"
+    end
+  end
+
   def run
     puts "Welcome to the JSL Twitter Client!"
     command = ""
@@ -63,7 +79,8 @@ class MicroBlogger
         when 'elt' then everyones_last_tweet
         when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         when 's' then shorten(parts[1..-1].join(" ")) 
-        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))                 
+        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))  
+        when 'klout' then klout_score              
         when 'q' then puts "Goodbye!"
         else
           puts "Sorry, I don't know how to #{command}"
